bca8bf1b97b8c119400442db1877bc05a2822b99
Taylor Masiello <masiellotay@gmail.com>
2022-05-09 22:17:29 -0700

    First C program, HelloWorld but it's pizza :)

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("I like pizza!\n");
+    printf("It's really good!\n");
+
+    return 0; // returns 0 if program runs succesfully; will return 1 if there is an error
+}
